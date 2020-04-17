6f2adb17883ddf1747df17e6fcc9f9e660b93d95
Agent1998 <msurapit@gmail.com>
2020-04-17 11:51:17 +0700

    v2

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 const Activity = () => {
 
     return (
         <div>
             44
-            <a href=""><button >Home</button></a>
+            <a href="/"><button >Home</button></a>
             <a href="/about"><button >About</button></a>
             <a href="/activity"><button >Activity</button></a>
+            <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
         </div>
     );
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-
-
-
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 const Home = () => {
   return (
     <div>
     123
-    <a href=""><button >Home</button></a>
+            <a href="/"><button >Home</button></a>
             <a href="/about"><button >About</button></a>
             <a href="/activity"><button >Activity</button></a>
+            <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
     </div>
     
   );
