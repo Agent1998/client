06881b2b3f11827f50d65c07e8dd7f786e658ac6
Agent1998 <msurapit@gmail.com>
2020-04-17 10:28:29 +0700

    ลองสร้างหน้าแรก

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react'
 import Login from './components/Login'
+import Test from './components/Test'
 import './App.css'
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Login />
+       
       </div>
     )
   }
---
+++ src/components/Test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div>
+            <div>
+            <a href="/"><button >Home</button></a>
+            <a href="/about"><button >About</button></a>
+            <a href="/activity"><button >Activity</button></a>
+            </div>
+        </div>
+    )
+}
+
+export default Test;
