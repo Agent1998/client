99c2033e8fa0aa3510bfec06ee70b1208cbc36d1
Agent1998 <msurapit@gmail.com>
2020-04-18 14:10:13 +0700

    aa

--- src/components/Topbar2.js
+++ src/components/Topbar2.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import 'firebase/auth';
 import firebase from 'firebase'
 import Topbar from './Topbar';
+import './Topbar.css'
 
 const Topbar2 = () => {
 
     return (
-        <div>
-            <Topbar/>
-            <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+        <div className = 'Topbar'>
+           <div className="btn-topbar"><Topbar/></div> 
+           <div className="btn-signout"><button onClick={() => firebase.auth().signOut()}>Sign-out</button></div>
         </div>
     );
 
