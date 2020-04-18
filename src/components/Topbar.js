7b606f0f3dddfc7aeb8b8b5675d40565bc4122c0
Agent1998 <msurapit@gmail.com>
2020-04-18 12:53:47 +0700

    link

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import firebase from 'firebase';
+import { Link, } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 
 const Topbar = () => {
@@ -7,10 +8,9 @@ const Topbar = () => {
         <div>
             <div>
                 <div>              
-                <a href="/"><button >Home</button></a>
-                <a href="/about"><button >About</button></a>
-                <a href="/activity"><button >Activity</button></a>
-               
+                <Link to="/"><button >Home</button></Link> 
+                <Link to="/about"><button>About</button></Link>
+                 <Link to="/activity"><button >Activity</button></Link>
                     {/* <img src={firebase.auth().currentUser.photoURL} />
                     <div>{firebase.auth().currentUser.displayName}</div> */}
                     <Button onClick={() => firebase.auth().signOut()}>Sign-out</Button>
