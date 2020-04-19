a99b4b0508379c43923072070903b1d2e780098b
Agent1998 <msurapit@gmail.com>
2020-04-19 10:49:01 +0700

    เพิ่มรูป

--- src/components/Topbar2.js
+++ src/components/Topbar2.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'firebase/auth';
 import firebase from 'firebase'
-import Topbar from './Topbar';
 import './Topbar.css'
 import { Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,7 +18,7 @@ const Topbar2 = () => {
                     <Nav.Link href="/activity">Activity</Nav.Link>
                     <Nav.Link href="/about">About</Nav.Link>
                     {/* <Form inline> */}
-                    {/* <img id="photo" className="pic" src={firebase.auth().currentUser.photoURL}/> */}
+                    <img id="photo" className="pic" src={firebase.auth().currentUser.photoURL}/>
             <Nav.Link onClick={() => firebase.auth().signOut() } href="/">Signout
             </Nav.Link>
           {/* </Form> */}
